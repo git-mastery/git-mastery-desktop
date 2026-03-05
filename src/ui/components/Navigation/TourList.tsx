bb85b715af726ac68ba604f740b97ae702b29214
Marcus Soh <macussoh38@gmail.com>
2026-03-05 15:40:44 +0800

    feat: add chevron icon and change tooltip location

--- src/ui/components/Navigation/TourList.tsx
+++ src/ui/components/Navigation/TourList.tsx
@@ -1,8 +1,11 @@
-import { Button, Collapse, Flex, Stack, Text, Tooltip } from "@mantine/core"
+import { Box, Button, Collapse, Flex, Stack, Text, Tooltip } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
 import type { Lesson, Tour, TourData } from "../../../types/Tour"
 import { useCustomQuery } from "../../hooks/useCustomQuery"
 import { buildUrl, useWebContentsView } from "../../context/useWebContentsView"
+import { IconChevronCompactDown, IconChevronDown } from "@tabler/icons-react"
+
+import classes from './TourList.module.css'
 
 export const TourList = () => {
 
@@ -17,8 +20,8 @@ const TourItem = ({ tour, index }: { tour: Tour, index: number }) => {
   const [opened, { toggle }] = useDisclosure(false);
   const { navigate } = useWebContentsView();
 
-  return <Flex direction={"column"}>
-    <Tooltip label={tour.title} position="right" withArrow>
+  return <Flex direction={"column"} key={tour.title}>
+    <Tooltip label={tour.title} position="bottom-start" withArrow multiline w="200">
 
       <Button
         onClick={toggle}
@@ -38,17 +41,27 @@ const TourItem = ({ tour, index }: { tour: Tour, index: number }) => {
             },
           }
 
-        } >{tour.title} </Button>
-    </Tooltip>
+        } >
+        <Flex gap={4} align="center">
+          <Box style={{ flexShrink: 0 }}>
+
+            <IconChevronDown className={classes['icon']} style={{
+              transform: opened ? "rotate(180deg)" : "none"
+            }} size={12} />
+          </Box>
+          {tour.title}
+        </Flex>
+      </Button>
+    </Tooltip >
     <Collapse in={opened} p="8px" w={"100%"}>
       {buildLesson({ path: `lessons/trail/${tour.folder}`, title: "Tour Home" }, navigate)}
       {Object.values(tour.lessons).map(lesson => buildLesson(lesson, navigate))}
     </Collapse>
-  </Flex>
+  </Flex >
 }
 
 const buildLesson = (lesson: Lesson, navigate: (url: string) => void) => {
-  return <Button variant="subtle" color="dark" size="sm" w="100%"
+  return <Button key={lesson.path} variant="subtle" color="dark" size="sm" w="100%"
     onClick={() => navigate(buildUrl(lesson))}
     styles={
       {
